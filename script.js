1700d998dbe3bc62ee839f66a4d27e0459212564
Rohan Sharma <70716534+ron2111@users.noreply.github.com>
2022-05-25 15:50:46 +0530

    2 new func

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
 
-
-
-
 const mainPage = document.querySelector('.main-container')
-
-
+// const heading = document.querySelector('h1');
 const ipDisplay = document.querySelector('.ip-display')  
 const locationDisplay = document.querySelector('.location-display')
 const geoDisplay = document.querySelector('.geolocation-display')
+const postal = document.querySelector('.postal')
+const isp = document.querySelector('.internet-provider')
 
 const btn = document.querySelector('.lookup-btn');
 
 btn.onclick = function (event) {
     btn.style.display = 'none'
+    // heading.textContent= `IP-Lookup App`
     fetch('https://ipapi.co/json').then(response => response.json())
 	.then((response) => {
     ipDisplay.textContent= `IP Address: ${response.ip}`
-    locationDisplay.textContent= `Location: ${response.city}, ${response.region}, ${response.country_name}`
     geoDisplay.textContent= `Geo location: ${response.latitude}°N, ${response.longitude}°E`
+    locationDisplay.textContent= `Location: ${response.city}, ${response.region}, ${response.country_name}`
+    postal.textContent= `Postal Code: ${response.postal}`
+    isp.textContent= `Internet Provider: ${response.org}`
 
     }).catch(err => console.error(err));
 
  };
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Host': 'weatherinfo.p.rapidapi.com',
-// 		'X-RapidAPI-Key': 'af54be9f19bd4923a12125318222405'
-// 	}
-// };
-
-// fetch('http://api.weatherapi.com/v1/current.json', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
